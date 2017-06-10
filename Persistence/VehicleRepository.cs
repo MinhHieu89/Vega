1ac782a67cba3ed05bb32e54abc67ed8aaabbc9a
Minh Hieu <minh.hieu.89@outlook.com>
2017-06-10 12:58:14 +0700

    Apply paging on the server.

--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -57,6 +57,7 @@ namespace Vega.Persistence
             };
 
             query = query.ApplyOrdering(queryObj, columnsMap);
+            query = query.ApplyPaging(queryObj);
 
             return await query.ToListAsync();
         }
